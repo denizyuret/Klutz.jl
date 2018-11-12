8f55c55aeb729d4c3a9d67630157c07abfcd816f
Mike J Innes <mike.j.innes@gmail.com>
2018-11-12 16:42:17 +0000

    zygote benchmarks

--- src/bench.jl
+++ src/bench.jl
@@ -10,8 +10,14 @@ end
 
 j = `$(Base.julia_cmd()) -O3`
 
+@info "Running Baseline Benchmarks"
+run(`$j --project=src/baseline src/baseline/baseline.jl`)
+
 @info "Running Flux Benchmarks"
 run(`$j --project=src/flux src/flux/flux.jl`)
 
 @info "Running Knet Benchmarks"
 run(`$j --project=src/knet src/knet/knet.jl`)
+
+@info "Running Zygote Benchmarks"
+run(`$j --project=src/zygote src/zygote/zygote.jl`)
